f99b23fcffea158d78731176b11da9285538721e
Magdy <magdyedwar1996@gmail.com>
2024-05-28 15:07:06 -0500

    Organizing the repo

--- Prog2.java
+++
@@ -1,9 +0,0 @@
-public class Prog2 {
-    public static void main(String[] args) {
-        float x = 1.27f, y = 3.881f, z = 9.6f;
-        int sum = (int) (x + y + z);
-        System.out.println(sum);
-        int sum1 = Math.round((x + y + z));
-        System.out.println(sum1);
-    }
-}
