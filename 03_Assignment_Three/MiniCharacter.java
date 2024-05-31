d906a1b19c8d11f304d43ee0760326cda82b2f67
Magdy <magdiedwar47@gmail.com>
2024-05-31 17:11:18 -0500

    this is the second problem in the third assignment, to find the minimum character in a given string

--- 03_Assignment_Three/MiniCharacter.java
+++ 03_Assignment_Three/MiniCharacter.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class MiniCharacter {
+public class MiniCharacter {
+    public static void main(String[] args) {
+        char c =  getMiniCharacter("kel");
+        System.out.println(c);
+
+    }
+
+    public static char getMiniCharacter(String str){
+        return getMiniCharacter(str,str.charAt(0));
+
+    }
+
+    private static char getMiniCharacter(String str,char minChar ){
+        if(str.isEmpty() ){
+            return minChar ;
+        }
+        if( str.charAt(0) < minChar  ){
+            minChar = str.charAt(0);
+        }
+        return getMiniCharacter(str.substring(1), minChar);
+
+    }
 }
