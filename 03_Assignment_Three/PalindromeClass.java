01d56ddd9fc5bd3b0618445e9813bd1a43152928
Magdy <magdiedwar47@gmail.com>
2024-05-31 20:01:15 -0500

    this is the fourth problem in the third assignment, to check if the number is plindrome of not using recursion

---
+++ 03_Assignment_Three/PalindromeClass.java
@@ -0,0 +1,27 @@
+public class PalindromeClass {
+    public static void main(String[] args) {
+
+        System.out.println(isNumberPalidrome(12444421));
+    }
+
+    public static boolean isNumberPalidrome(int number){
+        int result =  isPalinderHelper(number , 0);
+        return result == number ;
+
+    }
+
+
+
+    private static int  isPalinderHelper(int number , int reversed){
+        if(number == 0 ){
+            return  reversed ;
+
+        }
+        int lastDigit = number % 10 ;
+        reversed = reversed * 10 + lastDigit;
+        number = number / 10 ;
+
+        return isPalinderHelper(number, reversed);
+    }
+
+}
